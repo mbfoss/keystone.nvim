60f52d895c9ab2869dceb2038ac2949bf2f6a8a9
mbfoss <mbfoss@fastmail.com>
2026-07-02 17:11:54 +0200

    Update

--- lua/keystone/explore.lua
+++ lua/keystone/explore.lua
@@ -1,11 +1,12 @@
 local M = {}
 
 ---@class keystone.explore.Config
----@field unused any
+---@field marks_persist_file (string | fun():string)?  -- nil = stdpath("data")/keystone.exploremarks.json
 
 local function _get_default_config()
     ---@type keystone.explore.Config
     return {
+        marks_persist_file = nil,
     }
 end
 
@@ -15,6 +16,7 @@ M.config = _get_default_config()
 ---@param opts keystone.explore.Config?
 function M.setup(opts)
     M.config = vim.tbl_deep_extend("force", _get_default_config(), opts or {})
+    require("keystone.explore.marks").setup(M.config.marks_persist_file)
     require("keystone.tk.usercmd").register_user_cmd("FileSelector", function(cmd, args, opts)
             require("keystone.explore.command").run_command(cmd, args, opts)
         end,
