91299266c7f683ba3773ebb1606458697c8573dc
mbfoss <mbfoss@fastmail.com>
2026-09-17 14:09:38 +0200

    Update

--- lua/keystone/filetree/command.lua
+++ lua/keystone/filetree/command.lua
@@ -11,8 +11,6 @@ end
 function M.get_subcommands(cmd, rest, arg_lead)
     if cmd == "FileTree" then
         if #rest == 0 then
-            return { "open", "close", "toggle" }
-        elseif #rest == 1 and rest[1] == "open" then
             return vim.fn.getcompletion(arg_lead, "dir")
         end
     end
@@ -24,22 +22,16 @@ end
 ---@param opts vim.api.keyset.create_user_command.command_args
 function M.run_command(cmd, args, opts)
     if cmd == "FileTree" then
-        local command = args[1]
-        local name = args[2]
-        if command == nil or command == "" or command == "open" then
-            local dir = name and vim.fn.fnamemodify(name, ":p")
-            if dir and vim.fn.isdirectory(dir) == 0 then
-                vim.notify("Not a directory: " .. dir, vim.log.levels.ERROR)
-                return
-            end
-            _tree().open(dir)
-        elseif command == "close" then
-            _tree().close()
-        elseif command == "toggle" then
-            _tree().toggle()
-        else
-            vim.notify("Invalid Filetree command: " .. tostring(command))
+        if #args > 1 then
+            vim.notify("FileTree takes at most one directory", vim.log.levels.ERROR)
+            return
         end
+        local dir = args[1] and vim.fn.fnamemodify(args[1], ":p")
+        if dir and vim.fn.isdirectory(dir) == 0 then
+            vim.notify("Not a directory: " .. dir, vim.log.levels.ERROR)
+            return
+        end
+        _tree().open(dir)
     end
 end
 
