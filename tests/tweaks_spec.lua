05de6c10e9761ad51aa1a15015927aaad3e7e5e5
mbfoss <mbfoss@fastmail.com>
2026-09-19 11:12:48 +0200

    feat: auto_nohlsearch in tweaks

--- tests/tweaks_spec.lua
+++ tests/tweaks_spec.lua
@@ -105,3 +105,47 @@ describe("tweaks behaviours", function()
     vim.fn.delete(file)
   end)
 end)
+
+describe("tweaks auto_nohlsearch", function()
+  ---@param triggers keystone.tweaks.NohlsearchTriggers
+  local function setup(triggers)
+    tweaks.setup({ auto_nohlsearch = true, auto_nohlsearch_triggers = triggers })
+    vim.cmd.enew({ bang = true })
+    vim.api.nvim_buf_set_lines(0, 0, -1, false, { "one foo two foo", "three" })
+    vim.o.hlsearch = true
+    vim.fn.setreg("/", "foo")
+    vim.v.hlsearch = 1
+  end
+  after_each(function()
+    tweaks.disable()
+    vim.cmd("silent! only")
+  end)
+
+  it("clears the highlight on insert", function()
+    setup({})
+    vim.api.nvim_exec_autocmds("InsertEnter", {})
+    vim.wait(20)
+    assert.equal(0, vim.v.hlsearch)
+  end)
+
+  it("keeps the highlight on insert when on_insert is off", function()
+    setup({ on_insert = false })
+    vim.api.nvim_exec_autocmds("InsertEnter", {})
+    vim.wait(20)
+    assert.equal(1, vim.v.hlsearch)
+  end)
+
+  it("clears the highlight on window change", function()
+    setup({ on_win_change = true })
+    vim.cmd.split()
+    vim.wait(20)
+    assert.equal(0, vim.v.hlsearch)
+  end)
+
+  it("keeps the highlight on window change by default", function()
+    setup({})
+    vim.cmd.split()
+    vim.wait(20)
+    assert.equal(1, vim.v.hlsearch)
+  end)
+end)
